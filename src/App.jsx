072cbffda67db9eb0267368d8d690d75d2c6dbaf
Jeric <marianovjeric@gmail.com>
2022-12-21 21:50:15 -0800

    using todo component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Todo from './components/Todo.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>My To Do List</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
