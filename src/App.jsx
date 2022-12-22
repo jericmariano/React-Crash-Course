b3bab2e4e06378869b40ee535431612a5907ba9e
Jeric <marianovjeric@gmail.com>
2022-12-21 22:21:12 -0800

    using props

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,22 @@ function App() {
     <div>
       <Title />
       <div className="todo__wrapper">
-        <Todo />
-        <Todo />
-        <Todo />
+        <Todo 
+        title="Finish Frontend Simplified"
+        paragraph="Code along with Frontend Simplfied step by step."
+        />
+        <Todo 
+        title="Finish Interview Section"
+        paragraph="Finish every interview question in the next 6 weeks."
+        />
+        <Todo 
+        title="Land a $100k job"
+        paragraph="Apply to 100 jobs"
+        />
       </div>
-      <Modal />
+      <Modal 
+      title = "Are you sure?"
+      />
     </div>
   );
 }
