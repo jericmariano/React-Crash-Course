ac33e15f27163d7170d55171955135573789e361
Jeric <marianovjeric@gmail.com>
2022-12-21 21:58:01 -0800

    created and imported my own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Todo from './components/Todo.jsx'
+import Title from './components/Title.jsx'
 
 function App() {
   return (
     <div>
-      <h1>My To Do List</h1>
+      <Title />
       <Todo />
       <Todo />
       <Todo />
