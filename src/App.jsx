9ed1237a22058795d76f03348324c4c63a4be0d1
Jeric <marianovjeric@gmail.com>
2022-12-23 22:56:54 -0800

    useState examples complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
 import Modal from "./components/Modal.jsx";
-import React, { useState } from 'react'
-import Counter from "./components/Counter.jsx"
+import React, { useState } from "react";
+import Counter from "./components/Counter.jsx";
 
 function App() {
-  return <Counter />
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
+
+  function onTodoDelete() {
+    setShowModal(true);
+    console.log("OTD");
+  }
+
+  function cancelModal() {
+    setShowModal(false);
+  }
+
+  function confirmModal() {
+    setShowModal(false);
+  }
 
   return (
     <div>
@@ -19,14 +31,20 @@ function App() {
             console.log(event.target.value);
           }}
         />
-        <button onClick = { () => setShowModal(true)}>Add todo</button>
+        <button onClick={() => setShowModal(true)}>Add todo</button>
       </div>
       <div className="todo__wrapper">
-        <Todo title="Finish Frontend Simplified" />
-        <Todo title="Finish Interview Section" />
-        <Todo title="Land a $100k job" />
+        <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified" />
+        <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section" />
+        <Todo onTodoDelete={onTodoDelete} title="Land a $100k job" />
       </div>
-      {showModal && <Modal title="Are you sure?" />}
+      {showModal && (
+        <Modal
+          cancelModal={cancelModal}
+          confirmModal={confirmModal}
+          title="Are you sure?"
+        />
+      )}
     </div>
   );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,11 @@
 import './Todo.css'
 
-function Todo({ title }) {
+function Todo({ title, onTodoDelete }) {
 
-    function deleteTodo() {
-        console.log('deleteTodo()', title)
-    }
-    
     return (
         <div className="todo">
         <p>{ title }</p>
-        <button onClick={() => deleteTodo(1)}>Delete</button>
+        <button onClick={onTodoDelete}>Delete</button>
       </div>
     )
 }
