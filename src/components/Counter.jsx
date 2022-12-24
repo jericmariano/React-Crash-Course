1def678ad6a2b8f936ef42553dcb442381ddb1c4
Jeric <marianovjeric@gmail.com>
2022-12-23 22:17:43 -0800

    changing objects in react

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
 
 function Counter() {
-    const [counter, setCounter] = useState(0)
-    function incrementCounter() {
-        setCounter((prevCounter) => prevCounter + 1)
-        setCounter((prevCounter) => prevCounter + 1)
-    }
+   const [cart, setCart] = useState({ item: "apple", quantity: 0})
 
-    function decrementCounter() {
-        setCounter((prevCounter) => prevCounter - 1)
-        setCounter((prevCounter) => prevCounter - 1)
-    }
+   function removeApple() {
+    setCart((prevCart) => ({
+        ... prevCart,
+        quantity: prevCart.quantity - 1
+    }))
+   }
+
+   function addApple() {
+    setCart(prevCart => ({
+        ...prevCart,
+        quantity: prevCart.quantity + 1,
+    }))
+   }
+
     return (
         <div>
-            <button onClick={decrementCounter}>-</button>
-            {counter}
-            <button onClick={incrementCounter}>+</button>
+            <button onClick={removeApple}>-</button>
+            {cart.quantity}
+            {cart.item}
+            <button onClick={addApple}>+</button>
         </div>
     )
-}
+    }
 
 export default Counter
