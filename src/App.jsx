599d3382c76ccec8ef13966f5e267d8a0f513030
Jeric <marianovjeric@gmail.com>
2022-12-23 21:58:30 -0800

    react hooks counter created

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,31 @@ import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
 import Modal from "./components/Modal.jsx";
+import React, { useState } from 'react'
+import Counter from "./components/Counter.jsx"
 
 function App() {
+  return <Counter />
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <div>
       <Title />
       <div>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }} />
-        <button>Add todo</button>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        <button onClick = { () => setShowModal(true)}>Add todo</button>
       </div>
       <div className="todo__wrapper">
         <Todo title="Finish Frontend Simplified" />
         <Todo title="Finish Interview Section" />
         <Todo title="Land a $100k job" />
       </div>
-      <Modal 
-      title = "Are you sure?"
-      />
+      {showModal && <Modal title="Are you sure?" />}
     </div>
   );
 }
